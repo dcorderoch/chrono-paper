838a94554b2ef3628e731f12798393c1ac960e40
El David Cordero Chavarría <dacoch215@estudiantec.cr>
2017-04-20 18:08:31 -0600

    small refactor of fixed point arithmetic code

--- PintOS/src/threads/fixed_point.c
+++ PintOS/src/threads/fixed_point.c
@@ -1,20 +1,20 @@
-#include "threads/fixed_point.h"
+#include "fixed_point.h"
 
-// Convert from integer to fixed point.
+/* Convert from integer to fixed point. */
 fixed_t
-from_int (int integer )
+fp_from_int (int integer )
 {
   return integer * (1 << FP_SHIFT_AMOUNT);
 }
 
-// Convert from fixed point to integer, rounding to zero.
+/* Convert from fixed point to integer, rounding to zero. */
 int
 from_fp_round_zero (fixed_t fp)
 {
-  return fp fp / (1 << FP_SHIFT_AMOUNT);
+  return fp / (1 << FP_SHIFT_AMOUNT);
 }
 
-// Convert from fixed point to integer, rounding to nearest integer.
+/* Convert from fixed point to integer, rounding to nearest integer. */
 int
 from_fp_round_nearest (fixed_t fp)
 {
@@ -22,56 +22,56 @@ from_fp_round_nearest (fixed_t fp)
                    : (fp - (1 << FP_SHIFT_AMOUNT))/(1 << FP_SHIFT_AMOUNT);
 }
 
-// Add two fixed point numbers.
-fixed_point
+/* Add two fixed point numbers. */
+fixed_t
 fp_addition (fixed_t fp1, fixed_t fp2)
 {
   return fp1 + fp2;
 }
 
-// Substract fp2 from fp1.
+/* Substract fp2 from fp1. */
 fixed_t
-fp_substraction (fixed_t fp1, fixed_t fp2)
+fp_minus_fp (fixed_t fp1, fixed_t fp2)
 {
   return fp1 - fp2;
 }
 
-// Add integer to fixed point.
+/* Add integer to fixed point. */
 fixed_t
 fp_add_int_and_fp (int integer, fixed_t fp)
 {
   return fp + integer * (1 << FP_SHIFT_AMOUNT);
 }
 
-// Substract integer from fixed point.
+/* Substract integer from fixed point. */
 fixed_t
-fp_fp_minus_int (fixed_t fp, int integer)
+fp_minus_int (fixed_t fp, int integer)
 {
   return fp - integer * (1 << FP_SHIFT_AMOUNT);
 }
 
-// Multiply two fixed points.
+/* Multiply two fixed points. */
 fixed_t
 fp_times_fp (fixed_t fp1, fixed_t fp2)
 {
   return (fixed_t)(((int64_t) fp1) * fp2 / (1 << FP_SHIFT_AMOUNT));
 }
 
-// Multiply a fixed point and an integer.
+/* Multiply a fixed point and an integer. */
 fixed_t
 fp_times_int (fixed_t fp, int integer)
 {
   return fp * integer;
 }
 
-// Divide fp1 by fp2.
+/* Divide fp1 by fp2. */
 fixed_t
 fp_divided_by_fp (fixed_t fp1, fixed_t fp2)
 {
   return (fixed_t) (( (int64_t) fp1) * (1 << FP_SHIFT_AMOUNT) / fp2 );
 }
 
-// Divide fixed point by integer.
+/* Divide fixed point by integer. */
 fixed_t
 fp_divided_by_int (fixed_t fp, int integer)
 {
